de3da91efb3d35899eb3a1eb738afe89c17e1d83
nimisha-sara <nimishasg2001@gmail.com>
2024-04-02 13:40:46 +0530

    added docstrings

--- utils/pdf.py
+++ utils/pdf.py
@@ -1,13 +1,14 @@
-import fitz
 import re
+import fitz
 from tqdm import tqdm
 
 
 class PDF:
     """
-    Args:
-        file_paths (list): file paths of pdf files
+    Class to read and process PDF files.
 
+    Args:
+        file_paths (list): List of file paths of PDF files.
     """
 
     def __init__(self, file_paths: list):
@@ -15,6 +16,15 @@ class PDF:
         self.output_text = []
 
     def read_pdf(self, file: str) -> str:
+        """
+        Read text from a PDF file.
+
+        Args:
+            file (str): Path to the PDF file.
+
+        Returns:
+            str: Text extracted from the PDF.
+        """
         file_text = ""
 
         with fitz.open(file) as doc:
@@ -24,6 +34,15 @@ class PDF:
         return self.clean_text(file_text)
 
     def clean_text(self, text: str) -> str:
+        """
+        Clean the extracted text from a PDF.
+
+        Args:
+            text (str): Text extracted from the PDF.
+
+        Returns:
+            str: Cleaned text.
+        """
         lines = text.split("\n")
         non_empty_lines = "\n".join(filter(lambda line: line.strip() != "", lines))
         for char in ["(", ")", ",", "[", ";", "|"]:
@@ -32,18 +51,20 @@ class PDF:
         return self.handle_unicode(non_empty_lines)
 
     def get_hyperlinks(self, pdf_file: str) -> list:
-        """Gets all hyperlinks in a pdf file
+        """
+        Get hyperlinks from a PDF file
 
         Args:
-            pdf_file (str): File Path
+            pdf_file (str): Path to the PDF file
+
+        Returns:
+            list: List of hyperlinks found in the PDF
         """
         pdf_document = fitz.open(pdf_file)
-
         hyperlinks = []
 
         for page_num in range(len(pdf_document)):
             page = pdf_document.load_page(page_num)
-
             annotations = page.links()
 
             for annotation in annotations:
@@ -55,22 +76,19 @@ class PDF:
         return hyperlinks
 
     def handle_unicode(self, text: str) -> str:
+        """
+        Replace Unicode characters with their respective symbols.
+
+        Args:
+            text (str): Text to process.
+
+        Returns:
+            str: Processed text with Unicode characters replaced.
+        """
         replacements = {
-            "\u2013": "-",  # En Dash
-            "\u2014": "--",  # Em Dash
-            "\u2026": "...",  # Ellipsis
-            "\u2018": "'",  # Left Single Quotation Mark
-            "\u2019": "'",  # Right Single Quotation Mark
-            "\u201C": '"',  # Left Double Quotation Mark
-            "\u201D": '"',  # Right Double Quotation Mark
-            "\u00A0": " ",  # Non-breaking Space
-            "\u201a": ",",  # Single Low-9 Quotation Mark
-            "\u201e": '"',  # Double Low-9 Quotation Mark
-            "\u2022": "-",  # Bullet
-            "\u2013": "-",  # En Dash
-            "\u2014": "--",  # Em Dash
-            "\u2015": "--",  # Horizontal Bar
-            "\u2212": "-",  # Minus Sign
+            "\u2013": "-", "\u2014": "--", "\u2026": "...", "\u2018": "'", "\u2019": "'",
+            "\u201C": '"', "\u201D": '"', "\u00A0": " ", "\u201a": ",", "\u201e": '"',
+            "\u2022": "-", "\u2013": "-", "\u2014": "--", "\u2015": "--", "\u2212": "-"
         }
 
         pattern = re.compile("|".join(re.escape(key) for key in replacements.keys()))
@@ -78,6 +96,12 @@ class PDF:
         return processed_text
 
     def process_pdf(self):
+        """
+        Process PDF files.
+
+        Returns:
+            list: List containing dictionaries with status, text, and links for each processed PDF.
+        """
         for file in tqdm(self.file_paths):
             if file.split(".")[-1] == "pdf":
                 try:
@@ -89,7 +113,7 @@ class PDF:
                         }
                     )
                 except Exception as e:
-                    self.output_text.append({"status": False, "text": e})
+                    self.output_text.append({"status": False, "text": str(e)})
             else:
-                self.output_text.append({"status": False, "text": "not a .pdf file"})
+                self.output_text.append({"status": False, "text": "Not a .pdf file"})
         return self.output_text
