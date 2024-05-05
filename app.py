a7a39da2af02d367ff0f2d7ab0ddee905b84d339
nimisha-sara <nimishasg2001@gmail.com>
2024-05-05 19:44:00 +0530

    updated route

--- app.py
+++ app.py
@@ -43,16 +43,18 @@ async def resume_report(request: Request, file: UploadFile = File(...)):
 
     resume_text = [resume for resume in pdf_reader.process_pdf() if resume["status"]]
     ner = [CustomNER().process_text(resume["text"]) for resume in resume_text]
-    ner = {
-        key.lower(): [k.title() for k, v in ner[0].items() if v == key]
-        for key in set(ner[0].values())
-    }
+    # ner = {
+    #     key.lower(): [k.title() for k, v in ner[0].items() if v == key]
+    #     for key in set(ner[0].values())
+    # }
 
     job_role = JobClassifier().predict_job_role(resume_text[0]["text"])
 
-    words_to_replace = ner['skill'] + ner['org']
+    words_to_replace = ner["skill"] + ner["org"]
     words_to_replace = [re.escape(word) for word in words_to_replace]
-    replace_pattern = re.compile(r'\b(?:' + '|'.join(words_to_replace) + r')\b', flags=re.IGNORECASE)
+    replace_pattern = re.compile(
+        r"\b(?:" + "|".join(words_to_replace) + r")\b", flags=re.IGNORECASE
+    )
     resume_text[0]["text"] = replace_pattern.sub("", resume_text[0]["text"])
 
     links = " ".join(resume_text[0]["links"])
@@ -67,53 +69,62 @@ async def resume_report(request: Request, file: UploadFile = File(...)):
 
 
 @app.post("/recruiter/match")
-def resume_ranking(
+async def resume_ranking(
     request: Request,
     job_description: str = Form(...),
     action: str = Form(...),
-    files: Optional[list[UploadFile]] = File(...),
-    google_link: Optional[str] = File(...),
-    excel: Optional[UploadFile] = File(...)
+    pdf_file: list[UploadFile] = File(...),
+    google_link: str = File(...),
+    excel_file: UploadFile = File(...),
 ):
-    if action == 'upload':
-        if not files:
-            raise(Exception("File not Uploaded"))
+    print("\n\n========================================\n")
+    print(action, job_description, pdf_file, google_link, excel_file)
+    if action == "pdf_file":
+        if not pdf_file:
+            raise FileNotFoundError("PDF File not Uploaded")
         else:
             os.mkdir("./uploads")
             filenames = []
-            for file in files: # type: ignore
+            for file in pdf_file:
                 with open(f"uploads/{file.filename}", "wb") as buffer:
                     shutil.copyfileobj(file.file, buffer)
                 filenames.append(f"./uploads/{file.filename}")
 
-            pdf_reader = PDF(filenames).process_pdf()
-            resume_texts = [resume for resume in pdf_reader if resume["status"]]
-            error_files = [resume["filename"] for resume in pdf_reader if not resume["status"]]
-    elif action == 'excel':
-        if not excel:
-            raise(Exception("Excel File not Uploaded"))
-        else:
-            os.mkdir("./uploads")
-            with open(f"uploads/{excel.filename}", "wb") as buffer:
-                shutil.copyfileobj(excel.file, buffer)
-                filename = f"./uploads/{excel.filename}"
-            df = pd.read_excel(filename)
-            
-    elif action == 'google_link':
+            pdf_reader = PDF(filenames).process_pdf(path_type="file")
+    elif action == "excel_file":
+        if not excel_file:
+            raise FileNotFoundError("Excel File not Uploaded")
+
+        os.mkdir("./uploads")
+        with open(f"uploads/{excel_file.filename}", "wb") as buffer:
+            shutil.copyfileobj(excel_file.file, buffer)
+            filename = f"./uploads/{excel_file.filename}"
+
+        files = pd.read_excel(filename).iloc[:, 0].tolist()
+        pdf_reader = PDF(files).process_pdf(path_type="url")
+
+    elif action == "google_link":
         if not google_link:
-            raise(Exception("Google links not submitted"))
+            raise ValueError("Links not submitted")
         else:
-            pass
+            google_link = google_link.split(",")
+            pdf_reader = PDF(google_link).process_pdf(path_type="url")
     else:
         print(f"ERORRRRR: action is ==={action}===")
 
-    ranking = ResumeRanker().get_similarity(
-        job_description, resume_texts
-    )
-    
-    shutil.rmtree("uploads")
+    resume_texts = [resume for resume in pdf_reader if resume["status"]]
+    error_files = [
+        [resume["filename"], resume["text"]] for resume in pdf_reader if not resume["status"]
+    ]
+    print(f"\n\n========================================\n{resume_texts}\t{error_files}\n========================================\n")
+    ranking = ResumeRanker().get_similarity(job_description, resume_texts)
+
+    if action == "pdf_file":
+        shutil.rmtree("uploads")
+    print(f"\n\n========================================\n{ranking}\n========================================\n")
+
     return templates.TemplateResponse(
         request=request,
         name="recruiter-match.html",
-        context=ranking,
+        context={"ranking": ranking, "error": error_files},
     )
