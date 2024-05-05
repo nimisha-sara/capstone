e3172a114698c672dde09665c8169c8a918c6d36
nimisha-sara <nimishasg2001@gmail.com>
2024-05-05 14:10:36 +0530

    added fileId recognition

--- utils/pdf.py
+++ utils/pdf.py
@@ -1,4 +1,5 @@
 """This file if for pdf file or url processing"""
+
 import re
 import fitz
 import requests
@@ -44,16 +45,26 @@ class PDF:
             str: Processed text with Unicode characters replaced.
         """
         replacements = {
-            "\u2013": "-", "\u2014": "--", "\u2026": "...", "\u2018": "'", "\u2019": "'",
-            "\u201C": '"', "\u201D": '"', "\u00A0": " ", "\u201a": ",", "\u201e": '"',
-            "\u2022": "-", "\u2015": "--", "\u2212": "-"
+            "\u2013": "-",
+            "\u2014": "--",
+            "\u2026": "...",
+            "\u2018": "'",
+            "\u2019": "'",
+            "\u201C": '"',
+            "\u201D": '"',
+            "\u00A0": " ",
+            "\u201a": ",",
+            "\u201e": '"',
+            "\u2022": "-",
+            "\u2015": "--",
+            "\u2212": "-",
         }
 
         pattern = re.compile("|".join(re.escape(key) for key in replacements.keys()))
         processed_text = pattern.sub(lambda match: replacements[match.group(0)], text)
         return processed_text
-    
-    def read_pdf(self, file_path: str, path_type: str="file") -> str:
+
+    def read_pdf(self, file_path: str, path_type: str = "file") -> str:
         """
         Read text from a PDF file.
 
@@ -65,17 +76,17 @@ class PDF:
             str: Text extracted from the PDF.
         """
         file_text = ""
-        if path_type == 'file':
+        if path_type == "file":
             doc = fitz.open(filename=file_path, filetype="pdf")
-        elif path_type == 'url':
+        elif path_type == "url":
             res = requests.get(file_path, timeout=10)
-            doc = fitz.open(stream = res.content, filetype="pdf")
+            doc = fitz.open(stream=res.content, filetype="pdf")
         for page in doc:
             text = page.get_text()
             file_text += text
         return self.clean_text(file_text)
-    
-    def get_hyperlinks(self, file_path: str, path_type: str="file") -> list:
+
+    def get_hyperlinks(self, file_path: str, path_type: str = "file") -> list:
         """
         Get hyperlinks from a PDF file
 
@@ -86,11 +97,11 @@ class PDF:
         Returns:
             list: List of hyperlinks found in the PDF
         """
-        if path_type == 'file':
+        if path_type == "file":
             doc = fitz.open(filename=file_path, filetype="pdf")
-        elif path_type == 'url':
+        elif path_type == "url":
             res = requests.get(file_path, timeout=10)
-            doc = fitz.open(stream = res.content, filetype="pdf")
+            doc = fitz.open(stream=res.content, filetype="pdf")
 
         hyperlinks = []
 
@@ -106,8 +117,7 @@ class PDF:
         doc.close()
         return hyperlinks
 
-
-    def process_pdf(self, path_type: str="file"):
+    def process_pdf(self, path_type: str = "file"):
         """
         Process PDF files.
 
@@ -118,19 +128,28 @@ class PDF:
             list: List containing dictionaries with status, text, and links for each processed PDF.
         """
         for file in self.file_paths:
-            if file.split(".")[-1] == "pdf":
-                try:
-                    self.output_text.append(
-                        {
-                            "status": True,
-                            "text": self.read_pdf(file_path=file, path_type=path_type),
-                            "links": self.get_hyperlinks(file_path=file, path_type=path_type),
-                        }
-                    )
-                except Exception as e:
-                    self.output_text.append({"status": False, "text": str(e), "filename": file})
-            else:
-                self.output_text.append({"status": False, 
-                                         "text": "Not a .pdf file",
-                                         "filename": file})
+            if path_type == "url":
+                if "/file/" in file and "/view" in file:
+                    fileid = re.findall(pattern='[-\w]{25,}', string=file)[0]
+                    file = f"https://drive.google.com/uc?id={fileid}"
+                else:
+                    return {
+                        "status": False,
+                        "text": "Not a valid URL. Ensure that it is a drive link and has view access",
+                        "filename": file,
+                    }
+            try:
+                self.output_text.append(
+                    {
+                        "status": True,
+                        "text": self.read_pdf(file_path=file, path_type=path_type),
+                        "links": self.get_hyperlinks(
+                            file_path=file, path_type=path_type
+                        ),
+                    }
+                )
+            except Exception as e:
+                self.output_text.append(
+                    {"status": False, "text": str(e), "filename": file}
+                )
         return self.output_text
