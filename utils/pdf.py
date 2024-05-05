f192c4b273182050090031b7d27c38474aafa774
nimisha-sara <nimishasg2001@gmail.com>
2024-05-05 13:13:43 +0530

    updated to handle drive links

--- utils/pdf.py
+++ utils/pdf.py
@@ -1,6 +1,7 @@
-import fitz
+"""This file if for pdf file or url processing"""
 import re
-from tqdm import tqdm
+import fitz
+import requests
 
 
 class PDF:
@@ -14,24 +15,6 @@ class PDF:
     def __init__(self, file_paths: list):
         self.file_paths = file_paths
         self.output_text = []
-
-    def read_pdf(self, file: str) -> str:
-        """
-        Read text from a PDF file.
-
-        Args:
-            file (str): Path to the PDF file.
-
-        Returns:
-            str: Text extracted from the PDF.
-        """
-        file_text = ""
-
-        with fitz.open(file) as doc:
-            for page in doc:
-                text = page.get_text() # type: ignore
-                file_text += text
-        return self.clean_text(file_text)
 
     def clean_text(self, text: str) -> str:
         """
@@ -50,31 +33,6 @@ class PDF:
         non_empty_lines = non_empty_lines.replace("  ", " ")
         return self.handle_unicode(non_empty_lines)
 
-    def get_hyperlinks(self, pdf_file: str) -> list:
-        """
-        Get hyperlinks from a PDF file
-
-        Args:
-            pdf_file (str): Path to the PDF file
-
-        Returns:
-            list: List of hyperlinks found in the PDF
-        """
-        pdf_document = fitz.open(pdf_file)
-        hyperlinks = []
-
-        for page_num in range(len(pdf_document)):
-            page = pdf_document.load_page(page_num)
-            annotations = page.links()
-
-            for annotation in annotations:
-                hyperlink = annotation.get("uri")
-                if hyperlink:
-                    hyperlinks.append(hyperlink)
-
-        pdf_document.close()
-        return hyperlinks
-
     def handle_unicode(self, text: str) -> str:
         """
         Replace Unicode characters with their respective symbols.
@@ -88,32 +46,91 @@ class PDF:
         replacements = {
             "\u2013": "-", "\u2014": "--", "\u2026": "...", "\u2018": "'", "\u2019": "'",
             "\u201C": '"', "\u201D": '"', "\u00A0": " ", "\u201a": ",", "\u201e": '"',
-            "\u2022": "-", "\u2013": "-", "\u2014": "--", "\u2015": "--", "\u2212": "-"
+            "\u2022": "-", "\u2015": "--", "\u2212": "-"
         }
 
         pattern = re.compile("|".join(re.escape(key) for key in replacements.keys()))
         processed_text = pattern.sub(lambda match: replacements[match.group(0)], text)
         return processed_text
+    
+    def read_pdf(self, file_path: str, path_type: str="file") -> str:
+        """
+        Read text from a PDF file.
 
-    def process_pdf(self):
+        Args:
+            file_path (str): Path to the PDF file.
+            path_type (str): Type of file to be processed - 'file'(default), 'url'
+
+        Returns:
+            str: Text extracted from the PDF.
+        """
+        file_text = ""
+        if path_type == 'file':
+            doc = fitz.open(filename=file_path, filetype="pdf")
+        elif path_type == 'url':
+            res = requests.get(file_path, timeout=10)
+            doc = fitz.open(stream = res.content, filetype="pdf")
+        for page in doc:
+            text = page.get_text()
+            file_text += text
+        return self.clean_text(file_text)
+    
+    def get_hyperlinks(self, file_path: str, path_type: str="file") -> list:
+        """
+        Get hyperlinks from a PDF file
+
+        Args:
+            file_path (str): Path to the PDF file
+            type (str): Type of File to be processed - 'file'(default), 'url'
+
+        Returns:
+            list: List of hyperlinks found in the PDF
+        """
+        if path_type == 'file':
+            doc = fitz.open(filename=file_path, filetype="pdf")
+        elif path_type == 'url':
+            res = requests.get(file_path, timeout=10)
+            doc = fitz.open(stream = res.content, filetype="pdf")
+
+        hyperlinks = []
+
+        for page_num in range(len(doc)):
+            page = doc.load_page(page_num)
+            annotations = page.links()
+
+            for annotation in annotations:
+                hyperlink = annotation.get("uri")
+                if hyperlink:
+                    hyperlinks.append(hyperlink)
+
+        doc.close()
+        return hyperlinks
+
+
+    def process_pdf(self, path_type: str="file"):
         """
         Process PDF files.
+
+        Args:
+            path_type (str): Type of file to be processed - 'file'(default), 'url'
 
         Returns:
             list: List containing dictionaries with status, text, and links for each processed PDF.
         """
-        for file in tqdm(self.file_paths):
+        for file in self.file_paths:
             if file.split(".")[-1] == "pdf":
                 try:
                     self.output_text.append(
                         {
                             "status": True,
-                            "text": self.read_pdf(file),
-                            "links": self.get_hyperlinks(file),
+                            "text": self.read_pdf(file_path=file, path_type=path_type),
+                            "links": self.get_hyperlinks(file_path=file, path_type=path_type),
                         }
                     )
                 except Exception as e:
                     self.output_text.append({"status": False, "text": str(e), "filename": file})
             else:
-                self.output_text.append({"status": False, "text": "Not a .pdf file", "filename": file})
+                self.output_text.append({"status": False, 
+                                         "text": "Not a .pdf file",
+                                         "filename": file})
         return self.output_text
