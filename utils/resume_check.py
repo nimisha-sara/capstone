1339a2a2fa0d625b1a2329ae899937dc58242337
nimisha-sara <nimishasg2001@gmail.com>
2024-06-01 19:25:04 +0530

    altered style errors

--- utils/resume_check.py
+++ utils/resume_check.py
@@ -3,55 +3,142 @@ import nltk
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.tag import pos_tag
 import language_tool_python
-from spellchecker import SpellChecker
 
+# from spellchecker import SpellChecker
 
-nltk.download('averaged_perceptron_tagger')
-nltk.download('punkt')
+nltk.download("averaged_perceptron_tagger")
+nltk.download("punkt")
 
 
 class ResumeChecker:
     def __init__(self):
-        self.tool = SpellChecker()
+        self.tool = language_tool_python.LanguageTool("en-US")
 
         self.strong_action_verbs = [
-            "Accelerated", "Achieved", "Attained", "Completed", "Conceived", "Convinced", "Discovered",
-            "Doubled", "Effected", "Eliminated", "Expanded", "Expedited", "Founded", "Improved",
-            "Increased", "Initiated", "Innovated", "Introduced", "Invented", "Launched", "Mastered",
-            "Overcame", "Overhauled", "Pioneered", "Reduced", "Resolved", "Revitalized", "Spearheaded",
-            "Strengthened", "Transformed", "Upgraded", "Tripled", "Oversaw", "Lead", "Implemented",
-            "Negotiated", "Transformed", "Engineered", "Achieved", "Forecasted", "Administered",
-            "Analyzed", "Generated", "Devised", "Cultivated", "Coached", "Trained", "Guided",
-            "Directed", "Converted", "Created", "Designed", "Outlined", "Established", "Built",
-            "Constructed", "Collaborated", "Gathered", "Performed", "Executed", "Delivered", "Improved"
+            "Accelerated",
+            "Achieved",
+            "Attained",
+            "Completed",
+            "Conceived",
+            "Convinced",
+            "Discovered",
+            "Doubled",
+            "Effected",
+            "Eliminated",
+            "Expanded",
+            "Expedited",
+            "Founded",
+            "Improved",
+            "Increased",
+            "Initiated",
+            "Innovated",
+            "Introduced",
+            "Invented",
+            "Launched",
+            "Mastered",
+            "Overcame",
+            "Overhauled",
+            "Pioneered",
+            "Reduced",
+            "Resolved",
+            "Revitalized",
+            "Spearheaded",
+            "Strengthened",
+            "Transformed",
+            "Upgraded",
+            "Tripled",
+            "Oversaw",
+            "Lead",
+            "Implemented",
+            "Negotiated",
+            "Transformed",
+            "Engineered",
+            "Achieved",
+            "Forecasted",
+            "Administered",
+            "Analyzed",
+            "Generated",
+            "Devised",
+            "Cultivated",
+            "Coached",
+            "Trained",
+            "Guided",
+            "Directed",
+            "Converted",
+            "Created",
+            "Designed",
+            "Outlined",
+            "Established",
+            "Built",
+            "Constructed",
+            "Collaborated",
+            "Gathered",
+            "Performed",
+            "Executed",
+            "Delivered",
+            "Improved",
         ]
 
         self.personal_pronouns = [
-            "i", "you", "he", "she", "it", "we", "they", "me", "him", "her", "us", "them",
-            "my", "your", "his", "her", "its", "our", "their", "mine", "yours", "hers", "ours", "theirs"
+            "i",
+            "you",
+            "he",
+            "she",
+            "it",
+            "we",
+            "they",
+            "me",
+            "him",
+            "her",
+            "us",
+            "them",
+            "my",
+            "your",
+            "his",
+            "her",
+            "its",
+            "our",
+            "their",
+            "mine",
+            "yours",
+            "hers",
+            "ours",
+            "theirs",
         ]
 
-    def spell_check(self, text: str) -> list:
+    def grammar_check(self, text: str) -> list:
         """
-        Check grammar and spelling in the text
+        Check grammar in the text
 
         Args:
-            text (str): Text to perform grammar and spelling check on.
+            text (str): Text to perform grammar check on.
 
         Returns:
-            list: List of detected grammar and spelling errors.
+            list: List of detected grammatical errors.
         """
-        errors = {}
-        words = text.split()
-        misspelled = self.tool.unknown(words)
-        print(misspelled)
-        if misspelled:
-            for word in misspelled:
-                print(word)
-                try:
-                    errors[word] = list(self.tool.candidates(word))[:3]
-                except:
-                    pass
+        matches = self.tool.check(text)
+        errors = []
+        for match in matches:
+            match = list(match)
+            if (
+                match[0] != "MORFOLOGIK_RULE_EN_US"
+                and "whitespace" not in match[1]
+                and "consecutive spaces" not in match[1]
+            ):
+                if "Did you mean" in match[1]:
+                    if '"' in match[1].split("Did you mean")[-1]:
+                        message = match[1].split("Did you mean")[0]
+                else:
+                    message = match[1]
+                errors.append(
+                    {
+                        "message": message,
+                        "suggestions": match[2][:5],
+                        "error_start": match[3],
+                        "error_end": match[3] + match[6],
+                        "text": match[4],
+                    }
+                )
         return errors
 
     def check_action_verbs(self, text: str) -> dict:
@@ -67,7 +154,7 @@ class ResumeChecker:
         verbs = []
         tagged_words = pos_tag(word_tokenize(text))
         for word, pos in tagged_words:
-            if pos.startswith('VB'):
+            if pos.startswith("VB"):
                 verbs.append(word)
         strong_verbs = [verb for verb in verbs if verb in self.strong_action_verbs]
         return {"verbs": verbs, "strong_verbs": strong_verbs}
@@ -87,7 +174,7 @@ class ResumeChecker:
         for sentence in sentences:
             tagged_words = pos_tag(word_tokenize(sentence))
             for i in range(len(tagged_words) - 1):
-                if tagged_words[i][1] == 'VBN' and tagged_words[i+1][0] == 'by':
+                if tagged_words[i][1] == "VBN" and tagged_words[i + 1][0] == "by":
                     passive_sentences.append(sentence)
                     break
         return passive_sentences
@@ -102,16 +189,23 @@ class ResumeChecker:
         Returns:
             dict: Dictionary with platform name as key and link as value.
         """
-        platforms = ["linkedin", "github", "medium", "stackoverflow",
-                     "codechef", "codeforces", "dribble", "hackerrank"]
+        platforms = [
+            "linkedin",
+            "github",
+            "medium",
+            "stackoverflow",
+            "codechef",
+            "codeforces",
+            "dribble",
+            "hackerrank",
+        ]
         platform_links = {}
         for platform in platforms:
-            pattern = rf'\b(?:https?://)?(?:www\.)?{platform}\.com/\S*'
+            pattern = rf"\b(?:https?://)?(?:www\.)?{platform}\.com/\S*"
             match = re.search(pattern, text, re.IGNORECASE)
             if match:
                 platform_links[platform] = match.group(0)
         return platform_links
-
 
     def check_personal_pronouns(self, text: str) -> list:
         """
@@ -137,10 +231,10 @@ class ResumeChecker:
         Returns:
             bool: True if a references section is found, False otherwise.
         """
-        if re.search(r'\bReferences\b|\bReferees\b', text, re.IGNORECASE):
+        if re.search(r"\bReferences\b|\bReferees\b", text, re.IGNORECASE):
             return True
         return False
-    
+
     def perform_all_checks(self, text: str) -> dict:
         """
         Perform all checks on the given text.
@@ -152,11 +246,10 @@ class ResumeChecker:
             dict: Dictionary containing the results of all checks.
         """
         return {
-            "spell": self.spell_check(text),
+            "grammar": self.grammar_check(text),
             "action_verbs": self.check_action_verbs(text),
             "passive_language": self.check_passive_language(text),
             "footprint_links": self.check_digital_footprint_links(text),
             "personal_pronouns": self.check_personal_pronouns(text),
-            "references_section": self.check_references_section(text)
+            "references_section": self.check_references_section(text),
         }
-
